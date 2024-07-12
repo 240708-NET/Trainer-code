0e6c41d370910a6620bbae68ef0b860538d9578b
Richard Hawkins <Richard.Hawkins@revature.com>
2024-07-12 12:01:38 -0400

    add FileIO

---
+++ week1/FileIO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FileIO
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Again!");
+            string path = @"./TestText.txt";
+
+            string text = "some content for the file";
+
+            // read, write (overwrite content, or create the file), append (add to the end of the file)
+            if( !File.Exists( path ) )
+            {
+                File.WriteAllText( path, text );
+            }
+        }
+    }
+}
