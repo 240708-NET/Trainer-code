730603bcebf58924bfe23e7f721080c0c51b605e
Richard Hawkins <Richard.Hawkins@revature.com>
2024-07-19 12:27:21 -0400

    adding create and read methods to HelloEF

--- week2/HelloEF/Program.cs
+++ week2/HelloEF/Program.cs
@@ -8,10 +8,14 @@ namespace HelloEF
         public static void Main( string[] args )
         {
             Console.WriteLine( "Hello Again!" );
+            Pet MyPet = new Pet{ Name = "Claude", Cuteness = 10, Chaos = 1000, Species = "Hedgehog" };
 
-            Pet MyPet = new Pet{ Name = "Sil", Cuteness = 9, Chaos = 100, Species = "Cat" };
+            MyPet = CreatePet( MyPet );
+            // MyPet = GetPet ( MyPet );
+
             Console.WriteLine( MyPet.Speak() );
 
+// I struggled to get this bit working, but we found a way around it!
             //string ConnectionString = File.ReadAllText("./connectionstring");
 
             // DbContextOptionsBuilder<DataContext> ContextOptions = new DbContextOptionsBuilder<DataContext>().UseSqlServer(ConnectionString);
@@ -19,7 +23,49 @@ namespace HelloEF
             // - Either Or - 
             // DbContextOptions<DataContext> ContextOptions = new DbContextOptionsBuilder<DataContext>().UseSqlServer(ConnectionString).Options;
             // DataContext Context = new DataContext( ContextOptions );
-           
+        }
+
+        public static Pet GetPet( Pet MyPet )
+        {
+            using ( var context = new DataContext() )
+            {
+                //List<Pet> petList = context.Pets.ToList();
+                // LINQ
+                // Language Integrated Query
+                // we need an enumarable of things/objects to search through, and we can query against that enumerable.
+
+                var found = // could be a single thing, could be a collection...
+                    from p in context.Pets.ToList()
+                    where p.Name == MyPet.Name
+                        && p.Cuteness == MyPet.Cuteness
+                        && p.Chaos == MyPet.Chaos
+                        && p.Species == MyPet.Species
+                    select p;
+
+                return found.FirstOrDefault();
+            }
+        }
+
+        public static Pet GetPetById( int id )
+        {
+            using ( var context = new DataContext() )
+            {
+                Pet pet = context.Pets.Find(id);
+                return pet;
+            }
+        }
+
+        public static Pet CreatePet( Pet newPet )
+        {
+            using ( var context = new DataContext() )
+            {
+                context.Add(newPet); // we introduce the object to the context (local class)
+                var returned = context.SaveChanges(); // we send/communicate to the db
+
+                Console.WriteLine( returned );
+
+                return GetPet(newPet);
+            }
         }
     }
 
