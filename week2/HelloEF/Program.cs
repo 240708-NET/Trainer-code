a682b19faaa80ec22062aa1b565f49ef98489627
Richard Hawkins <Richard.Hawkins@revature.com>
2024-07-19 14:08:08 -0400

    update with new methods

--- week2/HelloEF/Program.cs
+++ week2/HelloEF/Program.cs
@@ -8,14 +8,26 @@ namespace HelloEF
         public static void Main( string[] args )
         {
             Console.WriteLine( "Hello Again!" );
-            Pet MyPet = new Pet{ Name = "Claude", Cuteness = 10, Chaos = 1000, Species = "Hedgehog" };
+            // Pet MyPet = new Pet{ Id = 3, Name = "Claude", Cuteness = 10, Chaos = 1000, Species = "Hedgehog" };
+            // MyPet.Speak();
 
-            MyPet = CreatePet( MyPet );
-            // MyPet = GetPet ( MyPet );
+            // Pet MyPet = CreatePet( MyPet );
+            // List<Pet> petList = GetAllPets();
+            // Pet returnedPet = GetPet( MyPet );
+            // Pet returnedPet = GetPetById( 3 );
+            // bool confirm = UpdatePet( MyPet );
+            // bool confirm = DeletePet( MyPet );
+            // bool confirm = DeletePetById( 2 );
+            // bool confirm = DeletePetByName( "Claude" );
+            // bool confirm = DeleteAllPets();
 
-            Console.WriteLine( MyPet.Speak() );
+            // Console.WriteLine( MyPet.Speak() );
+            // Console.WriteLine( returnedPet.Speak() );
+            // Console.WriteLine( confirm );
+            // foreach( var p in petList )
+            //     Console.WriteLine( p.Speak() );
 
-// I struggled to get this bit working, but we found a way around it!
+        // I struggled to get this bit working, but we found a way around it!
             //string ConnectionString = File.ReadAllText("./connectionstring");
 
             // DbContextOptionsBuilder<DataContext> ContextOptions = new DbContextOptionsBuilder<DataContext>().UseSqlServer(ConnectionString);
@@ -67,6 +79,82 @@ namespace HelloEF
                 return GetPet(newPet);
             }
         }
+
+        public static bool UpdatePet( Pet modifiedPet )
+        {
+            /*
+            Return a boolean true/false to confirm if the modified pet was saved to the database correctly. 
+            Accept the pet to be modified in the database, without changing the id of the existing entry in the database.
+
+            Cursor Parking Lot:_____
+            */
+
+
+            return false;
+        }
+
+        public static bool DeletePet( Pet removePet )
+        {
+            /*
+            Return a boolean true/false to confirm if the target pet was removed from the database correctly. 
+            Accept the pet to be removed from the database.
+
+            Cursor Parking Lot:_____
+            */
+
+
+            return false;
+        }
+
+        public static bool DeletePetById ( int id )
+        {
+            /*
+            Return a boolean true/false to confirm if the target pet was removed from the database correctly. 
+            Accept the id of the pet to be removed from the database.
+
+            Cursor Parking Lot:_____
+            */
+
+
+            return false;
+        }
+
+        public static bool DeletePetByName ( string petName )
+        {
+            /*
+            Return a boolean true/false to confirm if the target pet was removed from the database correctly. 
+            Accept the name of the pet to be removed from the database.
+
+            Cursor Parking Lot:_____
+            */
+
+
+            return false;
+        }
+
+        public static bool DeleteAllPets()
+        {
+            /*
+            Return a boolean true/false to confirm if all pets were removed from the database correctly.
+
+            Cursor Parking Lot:_____
+            */
+
+
+            return false;
+        }
+
+        public static List<Pet> GetAllPets()
+        {
+            /*
+            Return a list of all pets in the database. 
+
+            Cursor Parking Lot:_____
+            */
+
+
+            return new List<Pet>();
+        }
     }
 
     class Pet
@@ -85,7 +173,7 @@ namespace HelloEF
         // Methods
         public string Speak()
         {
-            return $"Hello, I'm {this.Name}!";
+            return $"Hello, I'm {this.Name}! I have Id #{this.Id}, I'm {this.Cuteness} cute, {this.Chaos} chaotic, and a {this.Species}.";
         }
     }
 
