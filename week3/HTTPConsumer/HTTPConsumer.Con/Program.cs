167d5c4e4109e269c92f68453762de5dd7677a9c
Richard Hawkins <Richard.Hawkins@revature.com>
2024-07-23 14:51:29 -0400

    Add HTTP Consumer and API

---
+++ week3/HTTPConsumer/HTTPConsumer.Con/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+
+using HTTPConsumer.Models;
+
+namespace HTTPConsumer.Con
+{
+    class Program
+    {
+        public static async Task Main( string[] args )
+        {
+            Console.WriteLine( "Http Client is starting..." );
+
+            string uri = "https://jsonplaceholder.typicode.com/posts/";
+
+            HttpClient client = new HttpClient();
+
+            string response = await client.GetStringAsync(uri);
+
+            // Console.WriteLine( response );
+
+            List<Post> postList = JsonSerializer.Deserialize<List<Post>>(response);
+
+            foreach ( var i in postList )
+                Console.WriteLine( i.id + ": " + i.title );
+
+        }
+    }
+}
